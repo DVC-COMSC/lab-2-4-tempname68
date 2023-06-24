7f340618ebfd186479e9ab5095f033790a542b2d
tempname68 <136948032+tempname68@users.noreply.github.com>
2023-06-24 02:50:30 +0000

    submitting string

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def main():
     ##################################################
     # Comlete your code here
+    original_str = "python programming"
+    sub1 = original_str[:6]
+    sub2 = original_str[7:18]
+    sub2 += " "
+    merged_str = sub2 + sub1
+    print(sub2)
+    print(sub1)
+    print(merged_str)
+
     ##################################################
     pass
 
